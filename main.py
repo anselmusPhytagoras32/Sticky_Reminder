c0e62ca2540bf4bf4d81eceea68e173b801cb928
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2025-10-20 00:03:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def add_message():
+
+
+def main():
+    while True:
+        print("Welcome to Sticky Reminder")
+        print("Select operation to perform")
+        print("1. Add a message")
+        print("2. Remove a message")
+        print("3. Exit")
+
+        choice  = input("Enter your choice: ")
