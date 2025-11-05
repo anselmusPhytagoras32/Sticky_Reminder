749b54b0be326299efc425ad40751bb1aa789a5d
gebhelAnselmusAntoninusPius <gebhelanselmlibardo.santos24@bicol-u.edu.ph>
2025-11-05 17:21:00 +0800

    initial commit

--- src/main/main.py
+++ src/main/main.py
@@ -1,21 +1,9 @@
-import tkinter as tk
-import tkinter.ttk as ttk
+print("Welcome to Sticky Reminder")
+print("Select operation to perform")
+print("1. Add a message")
+print("2. Remove a message")
+print("3. Exit")
 
-
-def add_task():
-    user_input = input("Enter a task: ")
-
-
-
-    while True:
-        print("Welcome to Sticky Reminder")
-        print("Select operation to perform")
-        print("1. Add a message")
-        print("2. Remove a message")
-        print("3. Exit")
-
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            add_task()
+   
 
 
