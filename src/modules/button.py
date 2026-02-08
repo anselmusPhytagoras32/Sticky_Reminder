9ddd27ee89fbca2426e3ef8f9d167d5003f008ca
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-02-09 02:03:49 +0800

    fix: position of the buttons and entry fields

--- src/modules/button.py
+++ src/modules/button.py
@@ -6,18 +6,18 @@ def history_button(parent):
         new_win.title("New List")
         tk.Label(new_win, text="This is your new list window").pack(padx=20, pady=20)
 
-    button_history = tk.Button(parent, text="Add New List", fg="black", command=open_new_window)
-    button_history.pack(side=tk.LEFT, padx=5)
+    button_history = tk.Button(parent, text="Edit List", fg="black", command=open_new_window)
+    button_history.pack(side=tk.RIGHT, anchor="s")
 
 def add_button(parent):
     def add_list():
         new_list = tk.Entry(parent, width=7, textvariable=tk.StringVar())
-        new_list.pack(side=tk.RIGHT, padx=5)
+        new_list.pack(side=tk.TOP,anchor="center")
     
     button_add = tk.Button(parent, text="Add Task", fg="black", command=add_list)
-    button_add.pack(side=tk.RIGHT, padx=5 )
+    button_add.pack(side=tk.BOTTOM, anchor="s")
 
 def delete_button(parent):
     # Pass the actual frame instance (parent) instead of the class tk.Frame
     button_quit = tk.Button(parent, text="QUIT", fg="black", command=parent.winfo_toplevel().destroy)
-    button_quit.pack(side=tk.LEFT, padx=5)
+    button_quit.pack(side=tk.LEFT, anchor="s")
--- src/modules/window.py
+++ src/modules/window.py
@@ -3,15 +3,15 @@ from modules import button
 
 def app_window():
     window = tk.Tk()
-    
-    main_frame = tk.Frame(window)
-    main_frame.pack(pady=20)
 
     window.title("To do")
     window.minsize(200, 300)
     window.maxsize(300, 500)
     window.geometry("350x350")
     tk.Label(window, text = "To do: ").pack()
+    
+    main_frame = tk.Frame(window)
+    main_frame.pack(expand=True, fill="both", pady=20)
     
     button.history_button(main_frame)
     button.add_button(main_frame)
