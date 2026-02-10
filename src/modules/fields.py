418683ccfeee3ceb57d2bb39131ea3ce56fa10a1
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-02-10 23:27:34 +0800

    feat: added get_text method to output the entries from field

--- src/modules/fields.py
+++ src/modules/fields.py
@@ -4,8 +4,15 @@ class input_field:
     def __init__(self, parent):
         self.parent = parent
         self.entries = []
+        
+        self.my_label = tk.Label(parent, text='')
+        self.my_label.pack(pady=20)
 
-    def new_list(self):
+    def new_entry(self):
         new_list = tk.Entry(self.parent, width=100, textvariable=tk.StringVar())
         new_list.pack(side=tk.TOP,anchor="center")
         self.entries.append(new_list)
+
+    def get_text(self):
+        all_text = "".join(input.get() + "\n" for input in self.entries)
+        self.my_label.config(text=all_text)
